17d7e4b7666dd15d460e3a615f698e56228d3549
jj0313 <128872674+jj0313@users.noreply.github.com>
2026-05-10 16:20:22 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "LINE Stock AI is running!"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    return 'OK'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
